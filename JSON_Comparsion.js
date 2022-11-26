a5834d216248c018483d4044fa4db4b87584ad62
Prasanth-97 <115787721+Prasanth-97@users.noreply.github.com>
2022-11-26 22:24:45 +0530

    Update JSON_Comparsion.js

--- JSON_Comparsion.js
+++ JSON_Comparsion.js
@@ -1,11 +1,11 @@
 var obj1 = {
-    name: "person 1",
-    age: 5
+    "name": "person 1",
+    "age": "5"
 
 }
 var obj2 = {
-    age: 5,
-    name: "person 1"
+    "age": "5",
+    "name": "person 1"
 }
 
 let jsonEqual = (obj1, obj2) => {
